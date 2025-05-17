0732bae44dfc18f173470198e5c6f245ce180d64
YeeunJ <45876648+YeeunJ@users.noreply.github.com>
2025-05-17 16:36:44 +0900

    Time: 21 ms (15.47%), Space: 57 MB (84.26%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int prev = nums[0];
+
+        for(int i=1; i<nums.length; i++) {
+            if(prev == nums[i]) return true;
+            else {
+                prev = nums[i];
+            }
+        }
+        return false;
+    }
+}
